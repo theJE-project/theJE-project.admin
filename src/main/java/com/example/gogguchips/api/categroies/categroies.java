9a85b94a15a8877e6c04d4a62bd9080d3f7dc2b8
Soda <k.sua923@gmail.com>
2025-07-22 16:58:31 +0900

    feat. enter
    assss

--- src/main/java/com/example/gogguchips/api/categroies/categroies.java
+++ src/main/java/com/example/gogguchips/api/categroies/categroies.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 @Mapper
 public interface categroies {
-    List<categroiesData> parent();          // parent IS NULL 인 상위 카테고리
+    List<categroiesData> parent();
+    // parent IS NULL 인 상위 카테고리
+    // parent IS NULL 인 상위 카테고리
     List<categroiesData> children(Long id); // 특정 parent ID에 해당하는 자식 카테고리들
 }
