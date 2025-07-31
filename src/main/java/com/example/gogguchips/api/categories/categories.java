9078f3c0b8fd0bf2634956c95ad5cd3beaa1d876
DESKTOP-8FSFFJK\TJ <tjoeunAcademy>
2025-07-31 09:45:18 +0900

    파일 초기화

--- src/main/java/com/example/gogguchips/api/categories/categories.java
+++
@@ -1,8 +0,0 @@
-package com.example.gogguchips.api.categories;
-import org.apache.ibatis.annotations.Mapper;
-import java.util.List;
-
-@Mapper
-public interface categories {
-    List<categoriesData> allSelect();
-}
