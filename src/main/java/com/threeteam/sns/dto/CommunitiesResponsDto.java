57ea219a57c2f40df4c81393e1998ddfcc4204e0
huiwon013 <hui_0113@naver.com>
2025-08-07 11:17:03 +0900

    원본으로

--- src/main/java/com/threeteam/sns/dto/CommunitiesResponsDto.java
+++ src/main/java/com/threeteam/sns/dto/CommunitiesResponsDto.java
@@ -11,18 +11,40 @@ import java.util.List;
 public class CommunitiesResponsDto {
     private Long	id;
     private UsersResponsDto users;
+    private Long	categories;
+    private String	title;
     private String	content;
     private Long	count ;
+    private String hash ;
     private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+    private boolean	is_delete;
+    private boolean	is_visible;
     private List<ImagesDto> images;
     private List<TracksDto> musics;
+//
+//    public CommunitiesResponsDto(CommunitiesDto data, UsersResponsDto user,List<ImagesDto> images, List<TracksDto> musics) {
+//        this.id = data.getId();
+//        this.users = user;
+//        this.content = data.getContent();
+//        this.count = data.getCount();
+//        this.created_at = data.getCreatedAt();
+//        this.images = images;
+//        this.musics = musics;
+//    }
 
     public CommunitiesResponsDto(CommunitiesDto data, UsersResponsDto user,List<ImagesDto> images, List<TracksDto> musics) {
         this.id = data.getId();
         this.users = user;
+        this.categories = data.getCategories();
+        this.title = data.getTitle();
         this.content = data.getContent();
         this.count = data.getCount();
+        this.hash = data.getHash();
         this.created_at = data.getCreatedAt();
+        this.updated_at = data.getUpdatedAt();
+        this.is_delete = data.getIsDelete();
+        this.is_visible = data.getIsVisible();
         this.images = images;
         this.musics = musics;
     }
