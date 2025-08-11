83232d7dbfedf8e04fc191c79243584f1a754d6d
ShinA <leesinas@naver.com>
2025-08-11 10:56:59 +0900

    text 이미지 슈퍼베이스

--- src/main/java/com/threeteam/sns/config/WebConfig.java
+++ src/main/java/com/threeteam/sns/config/WebConfig.java
@@ -1,15 +1,17 @@
-package com.threeteam.sns.config;
+//package com.threeteam.sns.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        String uploadPath = System.getProperty("user.dir") + "/uploads/";
+//        registry.addResourceHandler("/uploads/**")
+//                .addResourceLocations("file:" + uploadPath);
+//    }
+//}
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String uploadPath = System.getProperty("user.dir") + "/uploads/";
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadPath);
-    }
-}
+// 슈퍼베이스로 이거 필요 없어짐
